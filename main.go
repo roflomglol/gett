90c19e17526a597ef7b4907b520bced0fe2bc99c
Sergey Yazan <sergey.yazan@gmail.com>
2017-10-18 16:05:10 +0300

    adds database orm and sets up connection

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func main() {
+	databaseURL := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open("postgres", databaseURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
