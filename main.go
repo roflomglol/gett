0b5494aeaa08ba283b8c0ee4e01014194c473906
Sergey Yazan <sergey.yazan@gmail.com>
2017-10-22 17:07:40 +0300

    fixes type in main

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
 
 	e := echo.New()
 	h := handlers.Handler{Db: database.Db}
-	e.GET("driver/:id", h.GetDriver)
-	e.POST("import", h.BatchCreateDrivers)
+	e.GET("/driver/:id", h.GetDriver)
+	e.POST("/import", h.BatchCreateDrivers)
 
 	serverPort := os.Getenv("SERVER_PORT")
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", serverPort)))
